a69e8159bc40dfa814b6fb94c21259ce5d8ef3bd
suliman <122719578+SulimanHakimi@users.noreply.github.com>
2023-07-11 11:37:05 +0430

    update close btn

--- mian.js
+++ mian.js
@@ -2,6 +2,28 @@ document.getElementById("btn").addEventListener("click", () => {
   const randomNumber = Math.floor(Math.random() * 16777215);
   let randomCode = "#" + randomNumber.toString(16);
   document.getElementById("nav").style.backgroundColor = randomCode;
-  // document.getElementById("color-txt").innerText = randomCode;
-  // navigator.clipboard.writeText(randomCode);
 });
+let btnRemove = document.getElementById("btnRemove");
+let btnRemove2 = document.getElementById("btnRemove2");
+let box1 = document.getElementById('imgRemve');
+let box2 = document.getElementById('imgRemve2');
+let homeSec = document.getElementById('home') 
+
+btnRemove.addEventListener('click',()=>{
+  box1.style.display = "none";
+  if(box1.style.display = "none"){
+    box2.style.width = "100%";
+    btnRemove2.style.display = "block"
+  }
+})
+btnRemove2.addEventListener('click',()=>{
+  box2.style.display = "none"
+  box1.style.display = "block"
+  box1.style.width = "100%";
+  if(box1.style.width = "100%"){
+    btnRemove.style.right = "12%"
+  }
+  if(box1.style.display == "none" || box2.style.display == "none"){
+homeSec.style.display = "none"
+  }
+})
